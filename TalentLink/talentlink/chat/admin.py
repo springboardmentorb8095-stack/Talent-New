8f8d8c29dcc4121cdc44a6cd5de9bfeac587f468
PashamDhanushReddy <pashamdhanushreddy@gmail.com>
2025-12-16 22:18:55 +0530

    Revert "week-1,2"
    This reverts commit e1f9f521763d64b9ed7493a4aacb11cd8bcc2896.

--- TalentLink/talentlink/chat/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sender', 'receiver', 'contract', 'timestamp')
-    search_fields = ('sender__username', 'receiver__username')

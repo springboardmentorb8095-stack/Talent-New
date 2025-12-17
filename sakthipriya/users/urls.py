cf5c2c25f025c32d20b7147c31baa990172cf007
Sundharpriya <sakthisundhar97@gmail.com>
2025-12-17 19:28:58 +0530

    deleting sakthipriya folder

--- sakthipriya/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name="user-register"),
-    path('login/', LoginView.as_view(), name="user-login"),
-]

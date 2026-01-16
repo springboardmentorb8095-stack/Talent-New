39f6ba1b868efe9a255f4fd4ac45c0d114d7a689
Sundharpriya <sakthisundhar97@gmail.com>
2026-01-16 21:34:21 +0530

    Updating the files till week 8  to deploy in render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,26 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api/",
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("access");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
 import axios from "axios";
 
+// Use relative URL in production, absolute URL in development
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: process.env.NODE_ENV === 'production' 
+    ? '/api/' 
+    : 'http://127.0.0.1:8000/api/',
 });
 
 api.interceptors.request.use((config) => {

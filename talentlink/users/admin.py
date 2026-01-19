d1885c37c772c762cca4caa8579a76e80621b8fe
dhivya01052004-droid <dhivya01052004@gmail.com>
2026-01-19 11:27:40 +0530

    Week 7

--- talentlink/users/admin.py
+++ talentlink/users/admin.py
@@ -49,27 +49,31 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
-from django.db import ProgrammingError, OperationalError
+from django.db import ProgrammingError, OperationalError, connection
+
+# Only run this if table exists
+try:
+    if "users_user" in connection.introspection.table_names():
+        User.objects.filter(email="").update(email=None)
+except (ProgrammingError, OperationalError):
+    pass
+
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     # Add 'role' column to the users list view
     list_display = BaseUserAdmin.list_display + ("role",)
+
+    # Optional: Allow filtering and searching by role
     list_filter = BaseUserAdmin.list_filter + ("role",)
     search_fields = BaseUserAdmin.search_fields + ("role",)
+
+    # Add 'role' to the edit user form
     fieldsets = BaseUserAdmin.fieldsets + (
         ("Additional Information", {"fields": ("role",)}),
     )
+
+    # Add 'role' to the "Add User" form
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
         ("Additional Information", {"fields": ("role",)}),
     )
-
-# -----------------------------
-# Safely update empty emails AFTER migrations
-# -----------------------------
-try:
-    # This will fail if the table doesn't exist yet
-    User.objects.filter(email="").update(email=None)
-except (ProgrammingError, OperationalError):
-    # Ignore errors on fresh databases
-    pass
